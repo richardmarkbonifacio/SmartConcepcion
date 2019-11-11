78c7c4cc83cf57733edcc2178a3b11676c5cbbea
Richard Mark Bonifacio <richardbonifacio1015@gmail.com>
2019-11-12 06:31:57 +0800

    Health record

--- SmartConcepcion/Scripts/custom/jquery-dialog.js
+++ SmartConcepcion/Scripts/custom/jquery-dialog.js
@@ -15,3 +15,21 @@
     $("#dlgNameSuggestion").parent().appendTo($("form:first"));
     $("#MainContent_hfFrom").val(from);
 }
+
+function openHealthRecord() {
+    $("#dlgHealthRecord").dialog({
+        height: "auto",
+        width: "400px",
+        modal: true,
+        show: {
+            effect: "drop",
+            duration: 500
+        },
+        hide: {
+            effect: "drop",
+            duration: 500
+        }
+    });
+    $("#dlgHealthRecord").parent().appendTo($("form:first"));
+    
+}
